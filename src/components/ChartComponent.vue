83e4a667848828083ee0e7b9aa2f453c6f0e4e29
Leonid <leo1133407@list.ru>
2020-11-27 13:52:23 +0300

    before merge

---
+++ src/components/ChartComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component({})
+  export default class ChartComponent extends Vue {
+    @Prop() width: number;
+    @Prop() height: number;
+    @Prop() minWidth: number;
+    @Prop() maxWidth: number;
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
