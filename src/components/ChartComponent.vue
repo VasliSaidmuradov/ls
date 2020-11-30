710e0e3f76f6cdd66086c74465536b1c6ebec8fa
Leonid <leo1133407@list.ru>
2020-11-30 12:15:28 +0300

    doing chart

--- src/components/ChartComponent.vue
+++ src/components/ChartComponent.vue
@@ -28,12 +28,14 @@
 <script lang="ts">
   import { Component, Prop, Vue } from 'vue-property-decorator';
   import {
+    axisBottom as d3AxisBottom,
+    axisLeft as d3AxisLeft,
+    axisRight as d3AxisRight,
     interpolateRgb as d3InterpolateRgb,
+    max as d3Max,
     scaleBand as d3ScaleBand,
     scaleLinear as d3ScaleLinear,
     select as d3Select,
-    axisLeft as d3AxisLeft,
-    axisBottom as d3AxisBottom
   } from 'd3';
 
   interface IRefs {
@@ -72,6 +74,8 @@
         return;
       }
 
+      // КОНСТАНТЫ
+      const maxValue = d3Max(currentData);
       const width = 500;
       const height = 500;
       const DURATION = 1000;
@@ -79,56 +83,84 @@
       const innerWidth = width - margin.left - margin.right;
       const innerHeight = width - margin.top - margin.bottom;
 
+      // НАЧАЛО
       const svgData = d3Select(wrapper)
         .selectAll('svg')
         .data(['dummy data']);
-      // enter clause
+
       const svgEnter = svgData.enter().append('svg');
       svgEnter.attr('width', width);
       svgEnter.attr('height', height);
 
       const gEnter = svgEnter.append('g').attr('class', 'bars-chart')
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
-      // merge clause
+
       const svgMerge = svgData.merge(svgEnter); // svg
       const gMerge = svgMerge.select('g.bars-chart');
 
+      // ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
       const indexes: number[] = Array.from({ length: currentData.length }, (el, index) => index);
       const x = d3ScaleBand().range([0, innerWidth]).domain(indexes);
-      const y = d3ScaleLinear().range([innerHeight, 0]).domain([0, 400]);
+      const y = d3ScaleLinear().range([innerHeight, 0]).domain([0, maxValue]);
       const xColor = d3ScaleLinear()
         .domain([indexes[0], indexes[indexes.length - 1]])
         .interpolate(d3InterpolateRgb)
         .range(['#8a0000', '#8ab200']);
 
+      // ПУНКТИРНЫЕ ЛИНИИ
+      gEnter.append('g')
+        .attr('class', 'y-right')
+        .call(d3AxisRight(y).tickSizeOuter(0).tickSizeInner(innerWidth));
+
+      const gY = gMerge.select('g.y-right');
+
+      gY.transition()
+        .duration(DURATION)
+        .call(d3AxisRight(y).tickSizeOuter(0).tickSizeInner(innerWidth));
+
+      gY.selectAll('line')
+        .attr('stroke', '#63C58A')
+        .attr('stroke-dasharray', '2, 2');
+      gY.selectAll('text').remove();
+
+      // RECT
       const rectData = gMerge.selectAll('rect').data(currentData);
-      // enter clause
+
       const rectEnter = rectData.enter()
         .append('rect')
         .attr('fill', (d: any, idx: any) => xColor(idx))
         .attr('x', (d: any, idx: any) => x(idx))
         .attr('y', (d: any) => y(d))
         .attr('width', innerWidth / currentData.length)
-        .attr('height', (d: any) => d);
-      // merge clause
+        .attr('height', (d: any) => innerHeight - y(d));
+
       const rectMerge = rectData.merge(rectEnter); // rect
       rectMerge
         .transition()
         .duration(DURATION)
         .attr('y', (d: any) => y(d))
-        .attr('height', (d: any) => d);
+        .attr('height', (d: any) => innerHeight - y(d));
 
-      // ось Y
-      // enter
+      // ОСЬ Y
       gEnter.append('g')
-        .attr('class', 'y')
-        .call(d3AxisLeft(y))
+        .attr('class', 'y-left')
+        .call(d3AxisLeft(y));
 
-      // merge
-      gMerge.select('g.y')
+      gMerge.select('g.y-left')
         .transition()
-      .duration(DURATION)
-        .call(d3AxisLeft(y))
+        .duration(DURATION)
+        .call(d3AxisLeft(y));
+
+      // ОСЬ Х
+      gEnter.append('g')
+        .attr('class', 'x')
+        .call(d3AxisBottom(x));
+
+      gMerge.select('g.x')
+        .attr('transform', `translate(0, ${innerHeight})`)
+        .transition()
+        .duration(DURATION)
+        .call(d3AxisBottom(x));
 
     }
 
