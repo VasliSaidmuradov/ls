aeafc5e4110676dd8e19506d0d23dce91e2a8f06
Leonid <leo1133407@list.ru>
2020-12-02 16:00:18 +0300

    refactor chart

--- src/components/ChartComponent.vue
+++ src/components/ChartComponent.vue
@@ -1,10 +1,116 @@
 <template>
-  <div ref="wrapper" class="chart__wrapper">
+  <div ref="wrapper" class="chart__wrapper"
+       :style="{maxWidth: width + 'px', border: '1px solid black'}"
+  >
+    <!--    <div class="chart-tooltip"-->
+    <!--         :style="{-->
+    <!--            opacity: 1,-->
+    <!--            left: +d3Select(this).attr('x') - 20 + 'px',-->
+    <!--            top :+d3Select(this).attr('y') + 110 + 'px'-->
+    <!--         }"-->
+    <!--    >-->
+    <!--      <span class="tooltip-text">{{d.value}} {{d.laboratory.units}}</span>-->
+    <!--      <span class="tooltip-text">{{format(prettyDate(d.date), 'd MMMM yyyy', { locale: RU })}}</span>-->
+    <!--    </div>-->
+
+    <svg
+        :viewBox="viewBox"
+    >
+      <g class="bars-chart" :transform="`translate(${margin.left}, ${margin.top})`">
+        <!--HORIZONTAL-LINES-->
+        <g class="y-horizontal"></g>
+
+<!--        &lt;!&ndash;REF-ZONE-MAX&ndash;&gt;-->
+<!--        <g class="ref-zone-max">-->
+<!--          <line-->
+<!--              v-for="(d, idx) in data.results"-->
+<!--              :key="idx"-->
+<!--              :x1="countRefX1(idx)"-->
+<!--              :x2="x(prettyDate(d.date))"-->
+<!--              :y1="y(d.analyzer.ranges.max)"-->
+<!--              :y2="y(d.analyzer.ranges.max)"-->
+<!--              stroke="#63C58A"-->
+<!--              stroke-dasharray="2, 2"-->
+<!--          >-->
+<!--          </line>-->
+<!--        </g>-->
+
+<!--        &lt;!&ndash;REF-ZONE-MIN&ndash;&gt;-->
+<!--        <g class="ref-zone-min">-->
+<!--          <line-->
+<!--              v-for="(d, idx) in data.results"-->
+<!--              :key="idx"-->
+<!--              :x1="countRefX1(idx)"-->
+<!--              :x2="x(prettyDate(d.date))"-->
+<!--              :y1="y(d.analyzer.ranges.min)"-->
+<!--              :y2="y(d.analyzer.ranges.min)"-->
+<!--              stroke="#63C58A"-->
+<!--              stroke-dasharray="2, 2"-->
+<!--          >-->
+<!--          </line>-->
+<!--        </g>-->
+
+        <!--RECT-->
+        <rect
+            v-for="(d, idx) in data.results"
+            :key="idx"
+            :x="x(prettyDate(d.date))"
+            :y="y(d.value)"
+            :fill="countColor(d)"
+            :width="5"
+            :height="innerHeight - y(d.value) - 5"
+            :rx="3"
+        ></rect>
+
+        <!--AXIS X-->
+        <g class="x" :transform="`translate(0, ${innerHeight})`"></g>
+
+        <!--CIRCLE ON RECT-->
+        <g class="g-circle">
+          <circle
+              v-for="(d, idx) in data.results"
+              :key="idx"
+              :cx="x(prettyDate(d.date)) + 2.5"
+              :cy="y(d.value) - 7"
+              :fill="countColor(d)"
+              :opacity="d.laboratory.name === 'ЛабСтори' ? '1' : '0'"
+              :r="3"
+          ></circle>
+        </g>
+
+        <!--STAR ON RECT-->
+        <g class="g-star">
+          <path
+              v-for="(d, idx) in data.results"
+              :key="idx"
+              :d="countDStar"
+              :transform="`translate(${x(prettyDate(d.date)) + 2.5}, ${y(d.value) - 7})`"
+              :fill="countColor(d)"
+              :opacity="d.laboratory.name === 'ЛабСтори' ? '0' : '1'"
+          >
+          </path>
+        </g>
+
+        <!--TEXT ON RECT-->
+        <g class="g-text">
+          <text
+              v-for="(d, idx) in data.results"
+              :key="idx"
+              :x="x(prettyDate(d.date)) + 2"
+              :y="y(d.value) - 20"
+              :fill="countColor(d)"
+              text-anchor="middle"
+          >
+            {{d.value}}
+          </text>
+        </g>
+      </g>
+    </svg>
   </div>
 </template>
 
 <script lang="ts">
-  import { Component, Prop, Vue } from 'vue-property-decorator';
+  import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
   import {
     axisBottom as d3AxisBottom,
     axisRight as d3AxisRight,
@@ -17,8 +123,8 @@
     timeFormatDefaultLocale as d3TimeFormatDefaultLocale,
   } from 'd3';
   import parse from 'date-fns/parse';
-  import format from 'date-fns/format';
-  import RU from 'date-fns/locale/ru';
+  // import format from 'date-fns/format';
+  // import RU from 'date-fns/locale/ru';
 
   interface IRefs {
     wrapper: HTMLElement;
@@ -28,260 +134,129 @@
   export default class ChartComponent extends Vue {
     $refs: IRefs & Vue['$refs'];
 
+    margin = {
+      left: 30,
+      right: 30,
+      bottom: 40,
+      top: 0,
+    };
+    data: Array<any> = [];
+
     @Prop() mainData: Array<any>;
     @Prop() dateRange: Array<any>;
+    @Prop({ default: 722 }) width: number;
+    @Prop({ default: 350 }) height: number;
 
-    mounted() {
-      this.updateChart(this.$refs.wrapper, this.mainData.results);
+    get viewBox() {
+      return `0 0 ${this.width} ${this.height}`;
     }
 
-    updateChart(wrapper: HTMLElement, currentData: any) {
-      if (!wrapper) {
-        return;
-      }
+    get innerWidth() {
+      return this.width - this.margin.left - this.margin.right;
+    }
 
-      this.setLocale();
+    get innerHeight() {
+      return this.height - this.margin.top - this.margin.bottom;
+    }
 
-      // CONSTANTS
+    get valueArray() {
       const valueArray: number[] = [];
-      currentData.forEach((item: any) => {
+
+      this.data.results.forEach((item: any) => {
         valueArray.push(item.value);
         valueArray.push(item.analyzer.ranges.max);
       });
-      const maxValue = d3Max(valueArray);
-      const YMaxValue = maxValue + maxValue / 100 * 20;
-      const width = 500;
-      const height = 500;
-      const DURATION = 1000;
-      const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-      const innerWidth = width - margin.left - margin.right;
-      const innerHeight = width - margin.top - margin.bottom;
 
-      // BEGIN
-      const svgData = d3Select(wrapper)
-        .selectAll('svg')
-        .data(['dummy data']);
+      return valueArray;
+    }
 
-      const svgEnter = svgData.enter().append('svg');
-      svgEnter.attr('width', width);
-      svgEnter.attr('height', height);
+    get maxValue() {
+      return d3Max(this.valueArray);
+    }
 
-      const gEnter = svgEnter.append('g')
-        .attr('class', 'bars-chart')
-        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    get YMaxValue() {
+      return this.maxValue + this.maxValue / 100 * 20;
+    }
 
-      const svgMerge = svgData.merge(svgEnter);
-      const gMerge = svgMerge.select('g.bars-chart');
+    get rangeX() {
+      return [0, this.innerWidth];
+    }
 
-      // HELP FUNCTIONS
-      const x = d3ScaleTime()
-        .rangeRound([0, innerWidth])
-        .domain(this.dateRange);
+    get domainX() {
+      return this.dateRange;
+    }
 
-      const y = d3ScaleLinear()
-        .range([innerHeight, 0])
-        .domain([0, YMaxValue]);
+    get x() {
+      return d3ScaleTime()
+        .rangeRound(this.rangeX)
+        .domain(this.domainX);
+    }
 
-      const countColor = function(d: any): string {
-        const green = '#63C58A';
-        const red = '#FF7C7C';
-        const yellow = '#FFDE79';
-        const value = d.value;
-        const min = d.analyzer.ranges.min;
-        const max = d.analyzer.ranges.max;
+    get rangeY() {
+      return [this.innerHeight, 0];
+    }
 
-        if (value < min || value > max) {
-          return red;
-        } else if (value < (min + (min / 100 * 10)) || value > (max - (max / 100 * 10))) {
-          return yellow;
-        } else {
-          return green;
-        }
-      };
+    get domainY() {
+      return [0, this.YMaxValue];
+    }
 
-      const prettyDate = function(date: string): Date {
-        return parse(date, 'yyyy-MM-dd', new Date());
-      };
+    get y() {
+      return d3ScaleLinear()
+        .range(this.rangeY)
+        .domain(this.domainY);
+    }
 
-      // AXIS Х
-      gEnter.append('g')
-        .attr('class', 'x')
-        .call(d3AxisBottom(x));
+    get countDStar() {
+      return d3Symbol().type(d3SymbolStar).size(40)();
+    }
 
-      const gX = gMerge.select('g.x')
-        .attr('transform', `translate(0, ${innerHeight})`)
-        .transition()
-        .duration(DURATION)
-        .call(d3AxisBottom(x));
+    countRefX1(idx: number) {
+      if (this.data.results[idx + 1]) {
+        return this.x(this.prettyDate(this.data.results[idx + 1].date));
+      }
+    }
+
+    initAxisX() {
+      const gX = d3Select('.x')
+        .call(d3AxisBottom(this.x));
 
       gX.selectAll('line').remove();
       gX.select('.domain').remove();
-
-      // REF ZONES
-      // REF MAX
-      gEnter.append('g')
-        .attr('class', 'y-ref-max');
-
-      const gRefMax = gMerge.select('g.y-ref-max');
-
-      const refLineMax = gRefMax.selectAll('line').data(currentData);
-
-      const countRefX1 = function(idx: number) {
-        if (currentData[idx + 1]) {
-          return x(prettyDate(currentData[idx + 1].date));
-        }
-      };
-
-      refLineMax.enter()
-        .append('line')
-        .attr('x1', (d: any, idx: any) => countRefX1(idx))
-        .attr('x2', (d: any) => x(prettyDate(d.date)))
-        .attr('y1', (d: any) => y(d.analyzer.ranges.max))
-        .attr('y2', (d: any) => y(d.analyzer.ranges.max))
-        .attr('stroke', '#63C58A')
-        .attr('stroke-dasharray', '2, 2');
-
-      // REF MIN
-      gEnter.append('g')
-        .attr('class', 'y-ref-min');
-
-      const gRefMin = gMerge.select('g.y-ref-min');
-
-      const refLineMin = gRefMin.selectAll('line').data(currentData);
-
-      refLineMin.enter()
-        .append('line')
-        .attr('x1', (d: any, idx: any) => countRefX1(idx))
-        .attr('x2', (d: any) => x(prettyDate(d.date)))
-        .attr('y1', (d: any) => y(d.analyzer.ranges.min))
-        .attr('y2', (d: any) => y(d.analyzer.ranges.min))
-        .attr('stroke', '#63C58A')
-        .attr('stroke-dasharray', '2, 2');
-
-      // HORIZONTAL LINES
-      gEnter.append('g')
-        .attr('class', 'y-horizontal')
-        .call(d3AxisRight(y).tickSizeOuter(0).tickSizeInner(innerWidth));
-
-      const gYGreyHorizontal = gMerge.select('g.y-horizontal');
-
-      gYGreyHorizontal
-        .transition()
-        .duration(DURATION)
-        .call(d3AxisRight(y).ticks(3).tickSizeOuter(0).tickSizeInner(innerWidth));
-
-      gYGreyHorizontal.selectAll('line')
-        .attr('stroke', '#E9E8FF');
-      gYGreyHorizontal.selectAll('text').remove();
-      gYGreyHorizontal.select('.domain').remove();
-
-      // TOOLTIP
-      const tooltip = d3Select(wrapper)
-        .append('div')
-        .attr('class', 'chart-tooltip')
-        .style('opacity', 0);
-
-      // RECT
-      const rectData = gMerge.selectAll('rect').data(currentData);
-
-      const rectEnter = rectData.enter()
-        .append('rect')
-        .attr('class', 'rect')
-        .attr('fill', (d: any) => countColor(d))
-        .attr('x', (d: any) => x(prettyDate(d.date)))
-        .attr('y', (d: any) => y(d.value))
-        .attr('width', '5')
-        .attr('height', (d: any) => innerHeight - y(d.value))
-        .attr('rx', 3);
-
-
-      const rectMerge = rectData.merge(rectEnter);
-      rectMerge
-        .transition()
-        .duration(DURATION)
-        .attr('x', (d: any) => x(prettyDate(d.date)))
-        .attr('y', (d: any) => y(d.value))
-        .attr('height', (d: any) => innerHeight - y(d.value) - 5);
-
-      // RECT INTERACTIVE
-      rectEnter
-        .on('mouseenter', function(e: any, d: any) {
-          d3Select(this)
-            .attr('opacity', 0.5)
-            .attr('cursor', 'pointer');
-
-          tooltip.transition()
-            .duration(200)
-            .style('opacity', 0.9);
-
-          tooltip.html(`
-                      <span class="tooltip-text">${d.value} ${d.laboratory.units}</span>
-                      <span class="tooltip-text">${format(prettyDate(d.date), 'd MMMM yyyy', { locale: RU })}</span>
-          `);
-
-          tooltip
-            .style('left', +d3Select(this).attr('x') - 20 + 'px')
-            .style('top', +d3Select(this).attr('y') + 110 + 'px');
-        })
-        .on('mouseleave', function() {
-          d3Select(this)
-            .attr('opacity', 1);
-
-          tooltip.transition()
-            .duration(500)
-            .style('opacity', 0);
-        });
-
-      // CIRCLE ON RECT
-      gEnter.append('g')
-        .attr('class', 'g-circle');
-
-      const gCircle = gMerge.select('g.g-circle');
-
-      const circle = gCircle.selectAll('circle').data(currentData);
-
-      circle.enter()
-        .append('circle')
-        .attr('class', 'circle')
-        .attr('cx', (d: any) => x(prettyDate(d.date)) + 2.5)
-        .attr('cy', (d: any) => y(d.value) - 7)
-        .attr('r', '3')
-        .attr('fill', (d: any) => countColor(d))
-        .attr('opacity', (d: any) => d.laboratory.name === 'ЛабСтори' ? '1' : '0');
-
-      // STAR ON RECT
-      gEnter.append('g')
-        .attr('class', 'g-star');
-
-      const gStar = gMerge.select('g.g-star');
-
-      const star = gStar.selectAll('path').data(currentData);
-
-      star.enter()
-        .append('path')
-        .attr('class', 'star')
-        .attr('d', () => d3Symbol().type(d3SymbolStar).size(40)())
-        .attr('transform', (d: any) => `translate(${x(prettyDate(d.date)) + 2.5}, ${y(d.value) - 7})`)
-        .attr('fill', (d: any) => countColor(d))
-        .attr('opacity', (d: any) => d.laboratory.name === 'ЛабСтори' ? '0' : '1');
-
-      // TEXT ON RECT
-      gEnter.append('g')
-        .attr('class', 'g-text');
-
-      const gText = gMerge.select('g.g-text');
-
-      const text = gText.selectAll('text').data(currentData);
-
-      text.enter()
-        .append('text')
-        .attr('class', 'rect-value')
-        .attr('x', (d: any) => x(prettyDate(d.date)) + 2)
-        .attr('y', (d: any) => y(d.value) - 20)
-        .text((d: any) => d.value)
-        .attr('text-anchor', 'middle')
-        .attr('fill', (d: any) => countColor(d));
     }
+
+    initHorizontalLines() {
+      const gYHorizontal = d3Select('.y-horizontal')
+        .call(d3AxisRight(this.y).ticks(3).tickSizeOuter(0).tickSizeInner(this.innerWidth));
+
+      gYHorizontal.selectAll('line')
+        .attr('stroke', '#E9E8FF');
+      gYHorizontal.selectAll('text').remove();
+      gYHorizontal.select('.domain').remove();
+    }
+
+    countColor(d: any): string {
+      const green = '#63C58A';
+      const red = '#FF7C7C';
+      const yellow = '#FFDE79';
+      const value = d.value;
+      const min = d.analyzer.ranges.min;
+      const max = d.analyzer.ranges.max;
+      let returnColor = '';
+
+      if (value < min || value > max) {
+        returnColor = red;
+      } else if (value < (min + (min / 100 * 10)) || value > (max - (max / 100 * 10))) {
+        returnColor = yellow;
+      } else {
+        returnColor = green;
+      }
+
+      return returnColor;
+    }
+
+    prettyDate(date: string): Date {
+      return parse(date, 'yyyy-MM-dd', new Date());
+    };
 
     setLocale() {
       d3TimeFormatDefaultLocale({
@@ -299,16 +274,57 @@
         'shortMonths': ['Янв.', 'Фев.', 'Мар.', 'Апр.', 'Май.', 'Июн.', 'Июл.', 'Авг.', 'Сен.', 'Окт.', 'Ноя.', 'Дек.'],
       });
     }
+
+    init() {
+      this.initAxisX();
+      this.initHorizontalLines();
+    }
+
+    @Watch('mainData', { deep: true })
+    refreshChart(data: any) {
+      this.setLocale();
+      this.data = data;
+      this.init();
+    }
+
+    mounted() {
+      this.setLocale();
+      this.data = this.mainData;
+      this.init();
+    }
+
+    // RECT INTERACTIVE
+    // rectEnter
+    //   .on('mouseenter', function(e: any, d: any) {
+    //     d3Select(this)
+    //       .attr('opacity', 0.5)
+    //       .attr('cursor', 'pointer');
+    //
+    //     tooltip.transition()
+    //       .duration(200)
+    //       .style('opacity', 0.9);
+    //
+    //     tooltip.html(`
+    //                 <span class="tooltip-text">${d.value} ${d.laboratory.units}</span>
+    //                 <span class="tooltip-text">${format(prettyDate(d.date), 'd MMMM yyyy', { locale: RU })}</span>
+    //     `);
+    //
+    //     tooltip
+    //       .style('left', +d3Select(this).attr('x') - 20 + 'px')
+    //       .style('top', +d3Select(this).attr('y') + 110 + 'px');
+    //   })
+    //   .on('mouseleave', function() {
+    //     d3Select(this)
+    //       .attr('opacity', 1);
+    //
+    //     tooltip.transition()
+    //       .duration(500)
+    //       .style('opacity', 0);
+    //   });
   }
 </script>
 
 <style lang="scss" scoped>
-  .chart__wrapper {
-    /* mocks */
-    width: fit-content;
-    border: 1px solid black;
-  }
-
   ::v-deep text {
     font-weight: 500;
     font-size: 12px;
