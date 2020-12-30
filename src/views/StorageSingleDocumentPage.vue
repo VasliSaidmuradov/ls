e1f4b7b97b60dc20c64a3de10c29601caadccfaa
Leonid <leo1133407@list.ru>
2020-12-30 15:57:05 +0300

    added tableanalyzes on StorageSingleDocumentPage

--- src/views/StorageSingleDocumentPage.vue
+++ src/views/StorageSingleDocumentPage.vue
@@ -18,19 +18,87 @@
     <div class="document-page__file-wrapper">
       <file-item/>
     </div>
+
+    <div class="document-page__content-added" v-if="addedAnalyzes.length">
+      <span class="document-page__content-added-title">Добавлены</span>
+      <ul class="document-page__table-header" style="list-style: none;">
+        <li class="document-page__table-header-item">Биомаркер</li>
+        <li class="document-page__table-header-item">Значение</li>
+        <li class="document-page__table-header-item">Ед. изм.</li>
+        <li class="document-page__table-header-item">Референс. значения</li>
+        <li class="document-page__table-header-item">Лаборатория</li>
+        <li class="document-page__table-header-item">Дата</li>
+      </ul>
+      <ul class="document-page__content-added-list">
+        <li class="document-page__content-added-item" style="list-style: none;">
+          <StorageAnalyzesCard v-for="item in addedAnalyzes" :data="item" :key="item.id"/>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-  import {Component, Vue} from 'vue-property-decorator';
-  import InfoBlock from "@/components/storageSingleDocumentPage/InfoBlock.vue";
-  import FileItem from "@/components/storageSingleDocumentPage/FileItem.vue";
+  import { Component, Vue } from 'vue-property-decorator';
+  import InfoBlock from '@/components/storageSingleDocumentPage/InfoBlock.vue';
+  import FileItem from '@/components/storageSingleDocumentPage/FileItem.vue';
+  import { IAnalyzes } from '@/interfaces/analyzes.interface';
+  import StorageAnalyzesCard from '@/components/storagePage/StorageAnalyzesCard.vue';
 
   @Component({
-    components: {FileItem, InfoBlock}
+    components: { StorageAnalyzesCard, FileItem, InfoBlock },
   })
   export default class StorageSingleDocumentPage extends Vue {
-
+    addedAnalyzes: IAnalyzes.IAddedAnalyzes[] = [
+      {
+        id: Math.random().toString(36).substring(2, 7),
+        date: new Date(),
+        result: 43.54,
+        lab: 'Лаборатория',
+        name: 'повторяет функционал приведённого.',
+        ranges: {
+          min: 30,
+          max: null,
+        },
+        units: 'МЕ/л',
+      },
+      {
+        id: Math.random().toString(36).substring(2, 7),
+        date: new Date(),
+        result: 43.54,
+        lab: 'Лаборатория',
+        name: 'повторяет функционал приведённого.',
+        ranges: {
+          min: 30,
+          max: null,
+        },
+        units: 'МЕ/л',
+      },
+      {
+        id: Math.random().toString(36).substring(2, 7),
+        date: new Date(),
+        result: 43.54,
+        lab: 'Лаборатория',
+        name: 'повторяет функционал приведённого.',
+        ranges: {
+          min: 30,
+          max: null,
+        },
+        units: 'МЕ/л',
+      },
+      {
+        id: Math.random().toString(36).substring(2, 7),
+        date: new Date(),
+        result: 43.54,
+        lab: 'Лаборатория',
+        name: 'повторяет функционал приведённого.',
+        ranges: {
+          min: 30,
+          max: null,
+        },
+        units: 'МЕ/л',
+      },
+    ];
   }
 </script>
 
@@ -86,6 +154,61 @@
       margin-top: 30px;
       display: flex;
       align-items: center;
+    }
+
+    &__content-added {
+      margin-top: 75px;
+    }
+
+    &__table-header {
+      padding: 0;
+      margin: 0;
+      margin-top: 20px;
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+      font-size: 14px;
+      line-height: 130%;
+      color: $black-04;
+
+      @include media-breakpoint-up(1160px) {
+        display: none;
+      }
+    }
+
+    &__table-header-item {
+      &:first-of-type {
+        margin-left: -170px;
+      }
+
+      &:nth-of-type(2) {
+        margin-left: 20px;
+      }
+
+      &:nth-of-type(3) {
+        margin-left: -150px;
+      }
+
+      &:nth-of-type(4) {
+        margin-left: -150px;
+      }
+
+      &:nth-of-type(5) {
+        margin-left: -120px;
+      }
+    }
+
+    &__content-added-title {
+      color: $accent-color;
+      font-weight: 600;
+      font-size: 14px;
+      line-height: 130%;
+    }
+
+    &__content-added-list {
+      padding: 0;
+      margin: 0;
+      margin-top: 19px;
     }
   }
 </style>
